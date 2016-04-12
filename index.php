fab1b0b4a0b1dd5aa0285f5ed189d07ec1523242
Mbugua Gitau <mbugua.gitau@triad.co.ke>
2016-04-12 16:31:38 +0300

    my first commit

---
+++ index.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Plus2 Agency</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <!-- Custom -->
+    <link href="css/style.css" rel="stylesheet">
+  </head>
+  <body>
+    <header>
+      <div class="container-fluid">
+        <div class="main-banner">
+          <img src="img/banner.png" alt="banner" />
+        </div>
+        <div class="header-content">
+          <a href="#"><h1 class="logo">+2 Agency</h1></a>
+          <nav>
+            <h2>Main Navigation</h2>
+            <ul>
+              <li><a href="#">Work</a></li>
+              <li><a href="#">About</a></li>
+              <li><a href="#">Blog</a></li>
+              <li><a href="#">Contact</a></li>
+            </ul>
+          </nav>
+
+          <h1 class="header-title">PLUS2 AGENCY IS A SMALL INDEPENDENT WEB DEVELOPMENT <br> SHOP WORKING ON MAKING AWESOME WEB STUFF</h1>
+          <!-- <button class="header-btn" type="button" name="button">SEND ME A MESSAGE</button> -->
+    </div><!--end Container fluid --->
+    </div><!--end header-content-->
+    </header><!--end header-->
+
+    <!-- Start homepage content -->
+
+    <!--Start Services-->
+    <div class="container">
+      <h3 class="text-center">SERVICES</h3>
+        <div class="row">
+          <div class="col-md-4">
+            <img src="img/Design.png" alt="Design"/>
+            <h4 class="text-center">DESIGN</h4>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+              sed do eiusmod tempor incididunt ut labore et dolore magna
+              aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+              ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              Duis aute irure dolor in reprehenderit in voluptate velit
+              esse cillum dolore eu fugiat nulla pariatur. Excepteur
+              sint occaecat cupidatat non proident, sunt in culpa qui
+              officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+
+          <div class="col-md-4">
+            <img src="img/Develop.png" alt="launch"/>
+            <h4 class="text-center">DEVELOP</h4>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+              sed do eiusmod tempor incididunt ut labore et dolore magna
+              aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+              ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              Duis aute irure dolor in reprehenderit in voluptate velit
+              esse cillum dolore eu fugiat nulla pariatur. Excepteur
+              sint occaecat cupidatat non proident, sunt in culpa qui
+              officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+
+          <div class="col-md-4">
+            <img src="img/Launch.png" alt="Launch"/>
+            <h4 class="text-center">LAUNCH</h4>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+              sed do eiusmod tempor incididunt ut labore et dolore magna
+              aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+              ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              Duis aute irure dolor in reprehenderit in voluptate velit
+              esse cillum dolore eu fugiat nulla pariatur. Excepteur
+              sint occaecat cupidatat non proident, sunt in culpa qui
+              officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+          </div>
+        </div><!-- end container-->
+        <!-- end services -->
+
+
+        <!-- map section-->
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-md-12">
+              <img src="img/map.png" alt="map"/>
+            </div>
+          </div>
+        </div>
+
+
+
+
+    <!-- end homepage content -->
+
+    <footer>
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-4">
+            <h1 class="logo">+2 Agency</h1>
+          </div>
+          <div class="col-md-4">
+            <p>
+                hello@plus2.co.ke
+            </p>
+            <p>
+              (+254)729-072675
+            </p>
+          </div>
+          <div class="col-md-4">
+            <p>
+              Nairobi, kenya
+            </p>
+            <ul class="social_media">
+              <li class="twitter"><a href="#">twitter</a></li>
+              <li class="facebook"><a href="#">facebook</a></li>
+              <li class="github"><a href="#">github</a></li>
+              <li class="behance"><a href="#">behance</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
